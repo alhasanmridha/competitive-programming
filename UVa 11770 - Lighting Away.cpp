b15fc7fb3bc98b54c3544621ecfb1f00426966a6
MD Al-Hasan Mridha <mdalhasanmridha@gmail.com>
2017-04-10 16:34:54 +0600

    Create UVa 11770 - Lighting Away.cpp

---
+++ UVa 11770 - Lighting Away.cpp
@@ -0,0 +1,47 @@
+https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=2870
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <stack>
+using namespace std;
+vector<int> *v;
+stack<int>top_sort;
+bool vis[10001];
+void dfs(int indx){
+	vis[indx]=true;
+	for(int i=0;i<v[indx].size();i++){
+		if(!vis[v[indx][i]])
+			dfs(v[indx][i]);
+	}
+	top_sort.push(indx);
+}
+int main(){
+	int t,n,m,a,b,count,super;
+	scanf("%d",&t);
+	for(int it=1;it<=t;it++){
+		count=0;
+		scanf("%d%d",&n,&m);
+		v=new vector<int>[n];
+		for(int i=0;i<n;i++)
+			vis[i]=false;
+		for(int i=0;i<m;i++){
+			scanf("%d%d",&a,&b);
+			v[a-1].push_back(b-1);
+		}
+		for(int i=0;i<n;i++){
+			if(!vis[i])
+				dfs(i);
+		}
+		for(int i=0;i<n;i++)
+			vis[i]=false;
+		while(!top_sort.empty()){
+			super=top_sort.top();
+			if(!vis[super]){
+				dfs(super);
+				count++;
+			}
+			top_sort.pop();
+		}
+		cout<<"Case "<<it<<": "<<count<<"\n";
+	}
+}
