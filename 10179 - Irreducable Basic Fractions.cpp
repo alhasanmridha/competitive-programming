38ae1a134f1c7baf65fd5084ca84f3a08f3fcf4a
MD Al-Hasan Mridha <mdalhasanmridha@gmail.com>
2017-05-28 12:23:33 +0600

    Create 10179 - Irreducable Basic Fractions.cpp

---
+++ 10179 - Irreducable Basic Fractions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	while(scanf("%d",&n)&&n){
+		int res=n;
+		if(n%2==0){
+			res-=res/2;
+		}
+		while(n%2==0) n/=2;
+		for(int i=3;i*i<=n;i+=2){
+			if(n%i==0){
+				res-=res/i;
+				while(n%i==0) n/=i;
+			}
+		}
+		if(n>1){
+			res-=res/n;
+		}
+		printf("%d\n",res);
+	}
+}
