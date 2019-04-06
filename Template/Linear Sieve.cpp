1127ac666f0b989023c31c548ec9800eccd79b1d
MD Al-Hasan Mridha <mdalhasanmridha@gmail.com>
2019-04-06 11:07:36 +0600

    Update Linear Sieve.cpp

--- Template/Linear Sieve.cpp
+++ Template/Linear Sieve.cpp
@@ -1,18 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define M 1000005
-vector<int> prime;
+vector<int> primes;
 bool seive[M];
-int main(){
+void precalc_primes(){
 	for(int i=2;i<M;i++){
-		if(!seive[i]) prime.push_back(i);
-		for(int j=0;j<prime.size() and i*prime[j]<M;j++){
-			seive[prime[j]*i]=true;
-			if(i%prime[j]==0) break;
+		if(!seive[i]) primes.push_back(i);
+		for(int j=0;j<primes.size() and i*primes[j]<M;j++){
+			seive[primes[j]*i]=true;
+			if(i%primes[j]==0) break;
 		}
 	}
+
+}
+
+int main(){
+	precalc_primes();
 	for(int i=0;i<100;i++){
-		cout<<prime[i]<<" ";
+		cout<<primes[i]<<" ";
 	}
 	return 0;
 }
