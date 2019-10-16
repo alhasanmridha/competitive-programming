dd29b6ee4a43244695eee6553465ad31c0bdc203
MD Al-Hasan Mridha <mdalhasanmridha@gmail.com>
2019-10-17 01:37:05 +0600

    Create 11424 - GCD - Extreme (I).cpp

---
+++ Online Judge/11424 - GCD - Extreme (I).cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int LL;
+LL phi[1000001],lpf[1000001],sphi[1000002];
+vector<LL> primes;
+LL get_phi(LL a){
+	if(phi[a]!=0)
+		return phi[a];
+	LL p=lpf[a];
+	LL pw=1;
+	while(a%p==0){
+		pw*=p;
+		a/=p;
+	}
+	return phi[a*pw]=pw/p*(p-1)*get_phi(a);
+}
+int main(){
+	LL n;
+	phi[1]=1;
+	for(LL i=2;i<1000001;i++){
+		if(lpf[i]==0){
+			primes.push_back(i);
+			lpf[i]=i;
+		}
+		for(LL j=0;j<(LL)primes.size() and i*primes[j]<1000001 and primes[j]<=lpf[i];j++){
+			lpf[primes[j]*i]=primes[j];
+		}
+	}
+	for(int i=1;i<1000001;i++)
+		sphi[i]=sphi[i-1]+get_phi(i);
+	while(true){
+		scanf("%lld",&n);
+		if(n==0)
+			return 0;
+		LL ans=0;
+		for(LL l=1;l*l<=n;l++){
+			LL k=n/l;
+			ans+=(k*(k-1)*get_phi(l))/2;
+			if(k==l) break;
+			ans+=(l*(l-1)*(sphi[n/l]-sphi[n/(l+1)]))/2;
+		}
+		printf("%lld\n",ans);
+	}
+}
